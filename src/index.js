1d507cc3535839ee06b3e0a1b63c8e178320d495
gointoorbit <kania.anna@onet.eu>
2023-10-19 00:29:29 +0200

    new project structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import './js/api.js';
+import './js/mobile-menu.js';
+import './js/modal.js';
+import './js/shopping-list.js';
+import './js/support.js';
+import './js/theme-modes.js';
