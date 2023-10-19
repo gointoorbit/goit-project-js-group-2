b510ec7e90ede2f011e9c9dbf4bfb1bef460f1ff
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-10-19 23:57:20 +0200

    modal and random book

--- src/js/api.js
+++ src/js/api.js
@@ -1,31 +1,31 @@
-import axios from "axios";
+// import axios from "axios";
 
-const topBooks = "top-books";
-const categoryList = "category-list";
-let bookID; // ID książki
-let category; // nazwa kategorii
-const categorySelected = `category?category=${category}`; // zmienna do wywołania konkretnej kategorii
+// const topBooks = "top-books";
+// const categoryList = "category-list";
+// let bookID; // ID książki
+// let category; // nazwa kategorii
+// const categorySelected = `category?category=${category}`; // zmienna do wywołania konkretnej kategorii
 
-const getBooksApi = (value) => {
-    return axios
-    .get(`https://books-backend.p.goit.global/books/${value}`)
-    .then(respons => {
-        return respons
-    })
-}
+// const getBooksApi = (value) => {
+//     return axios
+//     .get(`https://books-backend.p.goit.global/books/${value}`)
+//     .then(respons => {
+//         return respons
+//     })
+// }
 
-export {getBooksApi, topBooks, categoryList, bookID, categorySelected, category};
+// export {getBooksApi, topBooks, categoryList, bookID, categorySelected, category};
 
-//-------------poniżej przykład wywołania funkcji pobierającej dane z api-------------//
+// //-------------poniżej przykład wywołania funkcji pobierającej dane z api-------------//
 
-getBooksApi(categoryList)
-    .then (api => {
-        return showMyCategory(api.data[0].list_name)
-    })
-    .catch(error => {
-        console.log(error)
-    })
+// getBooksApi(categoryList)
+//     .then (api => {
+//         return showMyCategory(api.data[0].list_name)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
 
-const showMyCategory = (myCategoryName) => {
-    console.log(myCategoryName, " <-- Przykład pobrania danych z API")
-}
+// const showMyCategory = (myCategoryName) => {
+//     console.log(myCategoryName, " <-- Przykład pobrania danych z API")
+// }
---
+++ src/js/modal.js
@@ -0,0 +1,68 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[btn-modal-open]"),
+      closeModalBtn: document.querySelector("[close-btn]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+
+
+
+  
+const randomBookContainer = document.querySelector('.random-book');
+const titleElement = randomBookContainer.querySelector('#title');
+const authorElement = randomBookContainer.querySelector('#author');
+const coverElement = randomBookContainer.querySelector('#cover');
+const buyLinkElement = randomBookContainer.querySelector('#buy-link');
+
+async function getRandomBook() {
+  try {
+    const response = await fetch('https://books-backend.p.goit.global/books/top-books');
+    if (response.ok) {
+      const data = await response.json();
+
+      // Losowy wybór kategorii i książki
+      const randomCategory = data[Math.floor(Math.random() * data.length)];
+      const randomBook = randomCategory.books[Math.floor(Math.random() * randomCategory.books.length)];
+     
+      coverElement.src = randomBook.book_image;  
+      titleElement.textContent = 'Tytuł: ' + randomBook.title;
+      authorElement.textContent = 'Autor: ' + randomBook.author;
+     
+      buyLinkElement.href = randomBook.amazon_product_url;
+      buyLinkElement.textContent = 'Kup teraz';
+    } else {
+      console.error('Wystąpił błąd podczas pobierania danych z API.');
+    }
+  } catch (error) {
+    console.error('Wystąpił błąd:', error);
+  }
+}
+
+getRandomBook();
+
+
+
+
+const randomBookElement = document.querySelector('.random-book');
+const bookElement = document.querySelector('.book');
+
+// Funkcja do kopiowania zawartości
+function copyRandomBookToBook() {
+  bookElement.innerHTML = randomBookElement.innerHTML;
+}
+
+// Obsługa przycisku "Open Modal"
+const modalButton = document.querySelector('.btn-modal-open');
+modalButton.addEventListener('click', function () {
+  // Wywołaj funkcję, aby skopiować zawartość ".random-book" do ".book"
+  copyRandomBookToBook()
+});
