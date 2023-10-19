5ffe7f79a3843dcf2843348f100a343515e2f357
raspiotr <raspiotr@gmail.com>
2023-10-19 18:12:04 +0200

    LocalStorage - save, load, remove using try...catch

---
+++ src/js/local-storage.js
@@ -0,0 +1,40 @@
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Remove state error: ', error.message);
+  }
+};
+
+export { save, load, remove };
+
+// Jak używać:
+// - zaimportować w swoim pliku js:
+// import * as localStorage from "/local-storage.js"
+// - wywołać, przykład:
+// const dog = {
+//   name: "Mango",
+//   age: 3,
+//   isHappy: true,
+// };
+
+// localStorage.save("dog", dog);
+// const doggy = localStorage.load("dog");
