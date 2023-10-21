0cb34e167628d77fc7a252cf9cd342eea5b81a8a
gointoorbit <kania.anna@onet.eu>
2023-10-21 19:17:19 +0200

    header-draft

--- src/js/theme-modes.js
+++ src/js/theme-modes.js
@@ -1,26 +1,33 @@
-const body = document.querySelector("body");
-let theme = localStorage.getItem("theme") || "light";
+const body = document.querySelector('body');
+let theme = localStorage.getItem('theme') || 'light';
 
 const ThemeMode = () => {
-    if (theme === "dark") {
-        body.classList.remove("darkmode")
-        theme = "light"
-    } else {
-        body.classList.add("darkmode");
-        theme = "dark"
-    }
-    localStorage.setItem("theme", theme)
-}
+  if (theme === 'dark') {
+    body.classList.remove('darkmode');
+    theme = 'light';
+    themeIndicator.classList.remove('dark-mode-on');
+  } else {
+    body.classList.add('darkmode');
+    theme = 'dark';
+    themeIndicator.classList.add('dark-mode-on');
+  }
+  localStorage.setItem('theme', theme);
+};
 
-
-if (theme === "dark") {
-    body.classList.add("darkmode")
+if (theme === 'dark') {
+  body.classList.add('darkmode');
+  themeIndicator.classList.add('dark-mode-on');
 }
 
 // narazie nie ma buttona od darkmoda więc darkmoda przełącza
 // sie tym 2 enterem tym prawym w rogu klawiatury
-document.addEventListener("keydown", event => {
-    if (event.code === "NumpadEnter") {
-        ThemeMode()
-    }
-})
+document.addEventListener('keydown', event => {
+  if (event.code === 'NumpadEnter') {
+    ThemeMode();
+  }
+});
+
+const themeSwitcher = document.querySelector('.theme-switcher');
+const themeIndicator = document.querySelector('.theme-switcher__toggler');
+
+themeSwitcher.addEventListener('click', ThemeMode);
