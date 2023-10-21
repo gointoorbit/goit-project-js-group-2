ea7d820478eb873592dcf2cbb6d1677674892918
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-10-21 11:35:24 +0200

    prototype modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -18,11 +18,11 @@
 
   
 const randomBookContainer = document.querySelector('.random-book');
-const titleElement = randomBookContainer.querySelector('#title');
-const authorElement = randomBookContainer.querySelector('#author');
-const descriptionElement = randomBookContainer.querySelector('#description')
-const coverElement = randomBookContainer.querySelector('#cover');
-const buyLinkElement = randomBookContainer.querySelector('#buy-link');
+const randomCoverElement = randomBookContainer.querySelector('#ran-cover');
+const randomTitleElement = randomBookContainer.querySelector('#ran-title');
+const randomAuthorElement = randomBookContainer.querySelector('#ran-author');
+const randomBoookDescriptionElement = randomBookContainer.querySelector('#ran-book-description');
+// const randomBookBuyLink = bookElement.querySelector('#ran-book-buy-link');
 
 async function getRandomBook() {
   try {
@@ -34,12 +34,11 @@ async function getRandomBook() {
       const randomCategory = data[Math.floor(Math.random() * data.length)];
       const randomBook = randomCategory.books[Math.floor(Math.random() * randomCategory.books.length)];
      
-      coverElement.src = randomBook.book_image;  
-      titleElement.textContent = 'Tytuł: ' + randomBook.title;
-      authorElement.textContent = 'Autor: ' + randomBook.author;
-      descriptionElement.textContent = 'Opis: ' + randomBook.description;  
-      buyLinkElement.href = randomBook.amazon_product_url;
-      buyLinkElement.textContent = 'Kup teraz';
+      randomCoverElement.src = randomBook.book_image;  
+      randomTitleElement.textContent = 'Tytuł: ' + randomBook.title;
+      randomAuthorElement.textContent = 'Autor: ' + randomBook.author;
+      console.log(randomBook);
+ 
     } else {
       console.error('Wystąpił błąd podczas pobierania danych z API.');
     }
@@ -51,19 +50,51 @@ async function getRandomBook() {
 getRandomBook();
 
 
+// Funkcja do tworzenia nowego diva .book z danymi z API
+function createBookDiv(selectedBook) {
+  const bookElement = document.getElementById('book');
+  const bookCover = bookElement.querySelector('#book-cover');
+  const bookTitleElement = bookElement.querySelector('#book-title');
+  const bookAuthorElement = bookElement.querySelector('#book-author');
+  const bookDescriptionElement = bookElement.querySelector('#book-description');
+  const bookBuyLink = bookElement.querySelector('#book-buy-link');
 
-
-const randomBookElement = document.querySelector('.random-book');
-const bookElement = document.querySelector('.book');
-
-// Funkcja do kopiowania zawartości
-function copyRandomBookToBook() {
-  bookElement.innerHTML = randomBookElement.innerHTML;
+  bookCover.src = selectedBook.book_image;
+  bookTitleElement.textContent = 'Tytuł: ' + selectedBook.title;
+  bookAuthorElement.textContent = 'Autor: ' + selectedBook.author;
+  bookDescriptionElement.textContent = 'Opis: ' + selectedBook.description;
+  bookBuyLink.href = selectedBook.amazon_product_url;
+  bookBuyLink.textContent = 'Kup teraz';
 }
 
-// Obsługa przycisku "Open Modal"
-const modalButton = document.querySelector('[btn-modal-open]');
-modalButton.addEventListener('click', function () {
-  // Wywołaj funkcję, aby skopiować zawartość ".random-book" do ".book"
-  copyRandomBookToBook()
+// Pobierz dane z API i wyświetl w divie .book
+async function fetchAndDisplayBook() {
+  try {
+    const response = await fetch('https://books-backend.p.goit.global/books/top-books');
+    if (response.ok) {
+      const data = await response.json();
+
+      // Losowy wybór kategorii i książki
+      const randomCategory = data[Math.floor(Math.random() * data.length)];
+      const randomBook = randomCategory.books[Math.floor(Math.random() * randomCategory.books.length)];
+
+      createBookDiv(randomBook); // Użyj danych z API do utworzenia diva .book
+    } else {
+      console.error('Wystąpił błąd podczas pobierania danych z API.');
+    }
+  } catch (error) {
+    console.error('Wystąpił błąd:', error);
+  }
+}
+
+// Obsługa kliknięcia na książkę
+const bookList = document.querySelectorAll('.random-book');
+bookList.forEach(book => {
+  book.addEventListener('click', function () {
+    fetchAndDisplayBook(); // Pobierz i wyświetl dane z API po kliknięciu w div .random-book
+  });
 });
+
+
+
+
