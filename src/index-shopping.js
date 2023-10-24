fca4d89031ac702dcc1f96d0ecfb64dcbb09b1ce
gointoorbit <kania.anna@onet.eu>
2023-10-24 15:52:55 +0200

    fixed theme mode

--- src/index-shopping.js
+++ src/index-shopping.js
@@ -3,6 +3,6 @@ import './js/api.js';
 import './js/mobile-menu.js';
 //import './js/modal.js';
 import './js/shopping-list.js';
-import './js/support.js';
+// import './js/support.js';
 import './js/theme-modes.js';
 //import './js/books-list.js';
