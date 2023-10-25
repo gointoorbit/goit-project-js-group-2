b447b6885fc9b1bf1129ab552dd39290daf755d7
gointoorbit <kania.anna@onet.eu>
2023-10-25 18:28:38 +0200

    switched on mobile menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import './js/api.js';
-// import './js/mobile-menu.js';
+import './js/mobile-menu.js';
 import './js/modal.js';
 import './js/support.js';
 import './js/theme-modes.js';
