e3081ba12a65cca59cdff45ec5a3036e6fab9ef0
gointoorbit <kania.anna@onet.eu>
2023-10-28 12:16:09 +0200

    marked current subpage

--- src/js/active-card.js
+++ src/js/active-card.js
@@ -1,29 +1,26 @@
-const logo = document.querySelector('header__logo-link');
-const brandName = document.querySelector('.header__brand-name-link');
-const home = document.querySelector('navigation__link-home');
-const shoppingList = document.querySelector('navigation__link-shopping-list');
-
-console.log(window.location.pathname);
-console.log(window.location.href);
-let pathToCompare;
 const currentSubpage = window.location.pathname;
-const currentPath = currentSubpage => {
-  const pathArray = currentSubpage.split('');
-  pathArray.unshift('.');
-  pathToCompare = pathArray.join('');
-  console.log(pathToCompare);
-  return pathToCompare;
-};
-currentPath(currentSubpage);
-console.log(pathToCompare);
-const nav = document.querySelectorAll('.navigation__link');
-console.log(nav);
 
-const linksPath = nav => {
-  nav.map(link => link.href);
-};
+const nav = document.querySelectorAll('.navigation--header__link');
+const navMobile = document.querySelectorAll('.navigation--mobile-menu__link');
+
+let linksPath = [];
+
+for (const link of nav) {
+  linksPath.push(link.getAttribute('href'));
+}
+
+let linksPathMobile = [];
+
+for (const link of navMobile) {
+  linksPathMobile.push(link.getAttribute('href'));
+}
+
 console.log(linksPath);
 
-const all = document.querySelector('nav a');
-console.log(all);
-console.log(all.href);
+const currentIndex = linksPath.indexOf(currentSubpage);
+console.log(currentIndex);
+
+const currentIndexMobile = linksPathMobile.indexOf(currentSubpage);
+
+nav[currentIndex].classList.add('navigation__link--current');
+navMobile[currentIndexMobile].classList.add('navigation__link--current');
