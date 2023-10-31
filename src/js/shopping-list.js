5761c8f6706d511aade54d605d31f226f8867c52
Mateusz Pawelec <m.pawelec01@gmail.com>
2023-10-31 03:01:41 +0100

    up-btn on shopping-list

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -133,3 +133,25 @@ const removeCardHandler = event => {
 
 // book counting function
 updateBookCount();
+
+// up-btn function
+const myButton = document.querySelector('.up-btn-shoplist');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    myButton.style.display = 'block';
+  } else {
+    myButton.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+myButton.addEventListener('click', topFunction);
